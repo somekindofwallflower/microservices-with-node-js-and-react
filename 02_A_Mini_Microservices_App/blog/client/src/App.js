c29a06a673c6c4a1f97b19ebb13549c80d9c7897
somekindofwallflower <someone.ana13@gmail.com>
2022-06-17 09:41:05 +0200

    Create basic structure of the application, Create post component containing create post form - add mthod to handle the submission

---
+++ 02_A_Mini_Microservices_App/blog/client/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PostCreate from "./PostCreate"
+const App = () => {
+    return <div className="container">
+        <h1>Create Post</h1>
+        <PostCreate/>
+    </div>;
+};
+
+export default App;
