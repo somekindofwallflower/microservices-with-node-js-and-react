99cfa211547d7554b1e944d4845172d7f3c7aabf
somekindofwallflower <someone.ana13@gmail.com>
2022-06-17 12:56:26 +0200

    Create Post list component

--- 02_A_Mini_Microservices_App/blog/client/src/App.js
+++ 02_A_Mini_Microservices_App/blog/client/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import PostCreate from "./PostCreate"
+import PostList from "./PostList";
 const App = () => {
     return <div className="container">
         <h1>Create Post</h1>
         <PostCreate/>
+        <h1>Posts</h1>
+        <PostList/>
     </div>;
 };
 
